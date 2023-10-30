a0c4adcd24d99ab138693cd1536047cfc3aafb01
Jhonn Vincent Arcipe <137320872+Waisnu@users.noreply.github.com>
2023-10-31 03:54:44 +0800

    Update Customer.java

--- AAACustomerServices/AAACustomerServices-api/src/main/java/AAACustomerServices/model/Customer.java
+++ AAACustomerServices/AAACustomerServices-api/src/main/java/AAACustomerServices/model/Customer.java
@@ -1,16 +1,4 @@
-/**
- * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- *
- * This library is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 2.1 of the License, or (at your option)
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
- * details.
- */
+
 
 package AAACustomerServices.model;
 
@@ -20,22 +8,11 @@ import com.liferay.portal.kernel.util.Accessor;
 
 import org.osgi.annotation.versioning.ProviderType;
 
-/**
- * The extended model interface for the Customer service. Represents a row in the &quot;FOO_Customer&quot; database table, with each column mapped to a property of this class.
- *
- * @author Brian Wing Shun Chan
- * @see CustomerModel
- * @generated
- */
+
 @ImplementationClassName("AAACustomerServices.model.impl.CustomerImpl")
 @ProviderType
 public interface Customer extends CustomerModel, PersistedModel {
 
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never modify this interface directly. Add methods to <code>AAACustomerServices.model.impl.CustomerImpl</code> and rerun ServiceBuilder to automatically copy the method declarations to this interface.
-	 */
 	public static final Accessor<Customer, Long> CUSTOMER_ID_ACCESSOR =
 		new Accessor<Customer, Long>() {
 
